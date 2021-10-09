7c4821fff3dc6d1bfbac5ddf76b7ff615b6a8b6f
orbeat <choiwonnyeong3032@gmai.com>
2021-10-09 14:53:40 +0900

    test

--- commit_and_push.cmd
+++ commit_and_push.cmd
@@ -7,9 +7,10 @@ git status
 git config --global user.email "choiwonnyeong3032@gmai.com"
 git config --global user.name "orbeat"
 
-git commit -m "asdf"
+set /p message=commit massage:
+git commit -m %message%
 git remote add origin https://github.com/orbeat/commit_test
 git remote
 
 git push origin master
-pause>nul
+pause
